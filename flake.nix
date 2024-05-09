59ed76d0610c6be8699c83b0f325b63a0003a1d0
Qiming Chu <cchuqiming@gmail.com>
2024-05-09 17:15:18 +0800

    Add nix fmt

--- flake.nix
+++ flake.nix
@@ -6,48 +6,57 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils }@inputs:
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  } @ inputs:
     flake-utils.lib.eachDefaultSystem
-      (system:
-      let
-        pkgs = import nixpkgs { inherit system;};
+    (
+      system: let
+        pkgs = import nixpkgs {inherit system;};
         deps = with pkgs; [
-	vim
-	tmux
-	yazi
-        git
-	man
-	ccache
+          vim
+          tmux
+          yazi
+          git
+          man
+          ccache
 
-        mill
-        dtc
-	boost
-        gnumake autoconf automake
-        cmake ninja
-        pkgsCross.riscv64-embedded.buildPackages.gcc
-        pkgsCross.riscv64-embedded.buildPackages.gdb
-        verilator cmake ninja
-        openocd
-        circt
-        spike
+          mill
+          dtc
+          boost
+          gnumake
+          autoconf
+          automake
+          cmake
+          ninja
+          pkgsCross.riscv64-embedded.buildPackages.gcc
+          pkgsCross.riscv64-embedded.buildPackages.gdb
+          verilator
+          cmake
+          ninja
+          openocd
+          circt
+          spike
 
-	llvm_17
-	SDL2
-	readline
+          llvm_17
+          SDL2
+          readline
         ];
-      in
-        {
-          legacyPackages = pkgs;
-          devShell = pkgs.mkShell.override { stdenv = pkgs.clangStdenv; } {
+      in {
+        legacyPackages = pkgs;
+        formatter = pkgs.alejandra;
+        devShell = pkgs.mkShell.override {stdenv = pkgs.clangStdenv;} {
           buildInputs = deps;
           SPIKE_ROOT = "${pkgs.spike}";
           RV64_TOOLCHAIN_ROOT = "${pkgs.pkgsCross.riscv64-embedded.buildPackages.gcc}";
           shellHook = ''
-		export PATH=$PATH:$SPIKE_ROOT/bin:$RV64_TOOLCHAIN_ROOT/bin
-		alias ra='yazi'
+            export PATH=$PATH:$SPIKE_ROOT/bin:$RV64_TOOLCHAIN_ROOT/bin
+            alias ra='yazi'
           '';
-          };
-        }
-      )
-    // { inherit inputs;};
+        };
+      }
+    )
+    // {inherit inputs;};
 }
