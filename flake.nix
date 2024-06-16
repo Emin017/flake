77b6f6ba52a46336e089d0cb1854e8bba6e403c2
Emin017 <cchuqiming@gmail.com>
2024-06-16 13:16:45 +0000

    darwin: Fix config naming error

--- flake.nix
+++ flake.nix
@@ -73,7 +73,7 @@
       ];
     };
     # Expose the package set, including overlays, for convenience.
-    darwinPackages = self.darwinConfigurations."MacBook-Air".pkgs;
+    darwinPackages = self.darwinConfigurations."MacBook".pkgs;
     formatter.aarch64-darwin = nixpkgs.legacyPackages.aarch64-darwin.alejandra;
     formatter.x86_64-linux = nixpkgs.legacyPackages.x86_64-linux.alejandra;
   };
