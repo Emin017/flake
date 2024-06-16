8ee19463c8d015951a0a242b720f6b25ded1801b
Emin017 <cchuqiming@gmail.com>
2024-06-16 13:09:43 +0000

    wsl: Add lazygit

--- system/linux/wsl/home.nix
+++ system/linux/wsl/home.nix
@@ -22,6 +22,8 @@
     xz
     unzip
 
+    lazygit
+
     ripgrep # recursively searches directories for a regex pattern
     jq # A lightweight and flexible command-line JSON processor
     yq-go # yaml processor https://github.com/mikefarah/yq
