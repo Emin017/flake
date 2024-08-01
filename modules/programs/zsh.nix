64ecc7e348b99138e4f4403791ceea50fac9f48e
Qiming Chu <cchuqiming@gmail.com>
2024-08-01 16:18:45 +0800

    modules: Enable autosuggestion in zsh

--- modules/programs/zsh.nix
+++ modules/programs/zsh.nix
@@ -16,6 +16,7 @@
   programs.zsh = {
     enable = true;
     enableCompletion = true;
+    autosuggestion.enable = true;
     initExtra = ''
       export PATH="$PATH:$HOME/bin:$HOME/.local/bin"
     '';
