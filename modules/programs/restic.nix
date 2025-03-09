4369f3cdcee2adda28cb882fdfe78398b4209a23
Qiming Chu <cchuqiming@gmail.com>
2025-03-09 17:23:51 +0800

    config(programs): add restic scripts for backup
    Signed-off-by: Qiming Chu <cchuqiming@gmail.com>

---
+++ modules/programs/restic.nix
@@ -0,0 +1,22 @@
+{ pkgs, lib, ... }: {
+  home.packages = [ pkgs.restic ];
+
+  home.file.".config/restic/restic-env".text = ''
+    export B2_ACCOUNT_ID=<YOUR_ACCOUNT_ID>
+    export B2_ACCOUNT_KEY=<YOUR_ACCOUNT_KEY>
+    export RESTIC_REPOSITORY=b2:<YOUR_BUCKET_NAME>:<YOUR_BUCKET_PATH>
+    export RESTIC_PASSWORD_FILE=~/.config/restic/restic-password
+  '';
+  home.file.".config/restic/restic-password".text = ''
+    YOUR_PASSWORD
+  '';
+  home.file.".config/restic/backup.sh".text = ''
+    #!/bin/env bash
+    restic backup --exclude-caches \
+    --exclude .Trash --exclude .DS_Store --exclude .localized \
+    --exclude .Spotlight-V100 --exclude .fseventsd \
+    --exclude .DocumentRevisions-V100 --exclude .MobileBackups \
+    --exclude .VolumeIcon.icns --exclude .PKInstallSandboxManager \
+    ~/Documents ~/Desktop ~/Downloads ~/Pictures ~/Movies
+  '';
+}
