d5b4ef4ba70ef837514b1304eee6bfed1d1c423c
Qiming Chu <cchuqiming@gmail.com>
2025-07-28 17:05:21 +0800

    config: add server home-manager configuration
    * Add home-manager configuration for server environment
    
    Signed-off-by: Qiming Chu <cchuqiming@gmail.com>

---
+++ system/linux/server/server.nix
@@ -0,0 +1,71 @@
+{ lib, config, pkgs, specialArgs, ... }: {
+  # Change your usrnanme and user directory here
+  home.username = lib.mkForce specialArgs.meta.hostname;
+  home.homeDirectory = lib.mkForce "/home/${specialArgs.meta.hostname}";
+
+  # Use home.packages to install packages
+  home.packages = with pkgs; [
+    neofetch
+    nnn
+
+    zip
+    xz
+    unzip
+
+    ripgrep # recursively searches directories for a regex pattern
+    jq # A lightweight and flexible command-line JSON processor
+    yq-go # yaml processor https://github.com/mikefarah/yq
+    eza # A modern replacement for ‘ls’
+    fzf # A command-line fuzzy finder
+
+    # networking tools
+    mtr # A network diagnostic tool
+    iperf3
+    dnsutils # `dig` + `nslookup`
+    ldns # replacement of `dig`, it provide the command `drill`
+    aria2 # A lightweight multi-protocol & multi-source command-line download utility
+    socat # replacement of openbsd-netcat
+    nmap # A utility for network discovery and security auditing
+    ipcalc # it is a calculator for the IPv4/v6 addresses
+
+    # nix related
+    #
+    # it provides the command `nom` works just like `nix`
+    # with more details log output
+    nix-output-monitor
+
+    # productivity
+    glow # markdown previewer in terminal
+    lazygit
+
+    btop # replacement of htop/nmon
+    iotop # io monitoring
+    iftop # network monitoring
+
+    # system call monitoring
+    strace # system call monitoring
+    ltrace # library call monitoring
+    lsof # list open files
+
+    wget
+    tree-sitter
+    yazi
+    nodejs
+    clang-tools
+    cmake
+  ];
+  imports = [ ./../../../modules/programs/zsh.nix ./../../../modules/neovim ];
+  # This value determines the Home Manager release that your
+  # configuration is compatible with. This helps avoid breakage
+  # when a new Home Manager release introduces backwards
+  # incompatible changes.
+  #
+  # You can update Home Manager without changing this value. See
+  # the Home Manager release notes for a list of state version
+  # changes in each release.
+  home.stateVersion = "25.05";
+
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+
+}
