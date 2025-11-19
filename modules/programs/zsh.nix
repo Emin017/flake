e27208691b0ff8e27da636f8b88a49e6dd62ac8a
Emin <me@emin.chat>
2025-11-19 15:11:54 +0800

    zsh: add newline
    Signed-off-by: Emin <me@emin.chat>

--- modules/programs/zsh.nix
+++ modules/programs/zsh.nix
@@ -3,10 +3,10 @@
   programs.starship = {
     enable = true;
     settings = {
-      add_newline = false;
+      add_newline = true;
       aws.disabled = true;
       gcloud.disabled = true;
-      line_break.disabled = true;
+      # line_break.disabled = true;
     };
   };
 
