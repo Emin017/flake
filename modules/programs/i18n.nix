95f3485578d34802141518f89bad660ca50390f1
Emin <me@emin.chat>
2025-11-19 15:18:33 +0800

    i18n: migrate deprecated option
    Signed-off-by: Emin <me@emin.chat>

--- modules/programs/i18n.nix
+++ modules/programs/i18n.nix
@@ -1,11 +1,12 @@
 { config, pkgs, ... }: {
   i18n.inputMethod = {
     enabled = "fcitx5";
-    fcitx5.addons = with pkgs; [
-      fcitx5-gtk # alternatively, kdePackages.fcitx5-qt
-      fcitx5-chinese-addons # table input method support
-      fcitx5-nord # a color theme
-      fcitx5-rime
-    ];
+    fcitx5.addons = with pkgs;
+      with qt6Packages; [
+        fcitx5-gtk # alternatively, kdePackages.fcitx5-qt
+        fcitx5-chinese-addons # table input method support
+        fcitx5-nord # a color theme
+        fcitx5-rime
+      ];
   };
 }
