246359b5a8aa217242cd8566052f48f61da47795
Qiming Chu <cchuqiming@gmail.com>
2025-11-19 19:48:55 +0800

    hydra: renmae nixos to fringe
    Signed-off-by: Qiming Chu <cchuqiming@gmail.com>

--- outputs/default.nix
+++ outputs/default.nix
@@ -154,7 +154,7 @@ parts.lib.mkFlake { inherit inputs; } {
     };
     # Import the hydra jobs
     hydraJobs = {
-      nixos = self.nixosConfigurations.nixos.config.system.build.toplevel;
+      fringe = self.nixosConfigurations.nixos.config.system.build.toplevel;
       wsl = self.nixosConfigurations.wsl.config.system.build.toplevel;
       hydra = self.nixosConfigurations.hydra.config.system.build.toplevel;
     };
